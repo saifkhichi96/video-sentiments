40e654700bc97bcadb93921760dade05b5429ab8
Saif Khan <saifkhichi96@gmail.com>
2021-01-15 02:04:20 +0100

    Improved transcription quality.

--- src/transcribe.py
+++ src/transcribe.py
@@ -1,6 +1,7 @@
 import io
 
-from google.cloud import speech
+# from google.cloud import speech
+from google.cloud import speech_v1p1beta1 as speech
 
 
 def transcribe(speech_file):
@@ -11,10 +12,20 @@ def transcribe(speech_file):
         content = audio_file.read()
 
     audio = speech.RecognitionAudio(content=content)
+    metadata = speech.RecognitionMetadata()
+    metadata.recording_device_type = (
+        speech.RecognitionMetadata.RecordingDeviceType.SMARTPHONE
+    )
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
         language_code="en-US",
         audio_channel_count=2,
+        enable_automatic_punctuation=True,
+        enable_speaker_diarization=True,
+        diarization_speaker_count=2,
+        use_enhanced=True,
+        model="video",
+        metadata=metadata,
     )
 
     response = client.recognize(config=config, audio=audio)
